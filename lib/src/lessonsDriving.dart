c5dc946e2576bcb5757b04b7280d560275b7ed59
sofiabukovskaya <sofi454545@gmail.com>
2021-04-13 15:49:53 +0300

    Create bottom navigation bar

--- lib/src/lessonsDriving.dart
+++ lib/src/lessonsDriving.dart
@@ -10,22 +10,47 @@ class AppState extends State<App> {
   Widget build(context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.white10),
+      theme: ThemeData(
+        primaryColor: Color(0xFF546E7A),
+        accentColor: Color(0xFF607D8B)
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text(
             'Lessons',
-            style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.wrap_text),
-          ),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: (){})
+          ],
         ),
-      bottomNavigationBar: BottomNavigationBar(
-
-      ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Color(0xFF546E7A),
+          selectedItemColor: Colors.amberAccent,
+          unselectedItemColor: Colors.white,
+          currentIndex: 0,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.book_outlined),
+                title: Text("")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                title: Text("")
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.money_outlined),
+              title: Text("")
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+                title: Text("")
+            )
+          ],
+        ),
       ),
     );
   }
